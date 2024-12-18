1cd063c3613b44a96754ad75055e9302cb6f6633
Rupam <rupammondal564@gmail.com>
2024-12-18 14:56:49 +0530

    Not found page implemented

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Auth from "./pages/auth"
 import SignUpCard from "./components/organisms/SingnUpcard"
 import SignInCard from "./components/organisms/Signincard"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { Notfound } from "./pages/Nofoundpage"
 function App() {
   const queryClient = new QueryClient();
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/auth/signup" element={<Auth><SignUpCard /></Auth>} />
           <Route path="/auth/signin" element={<Auth><SignInCard /></Auth>} />
+          <Route path="/*" element={<Notfound/>}/>
         </Routes>
       </QueryClientProvider>
   )
