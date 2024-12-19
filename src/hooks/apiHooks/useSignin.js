e14c750a956cc2047e470cf7446a1a2843322c2d
Rupam <rupammondal564@gmail.com>
2024-12-19 21:18:02 +0530

    Issue fixed of protected component

--- src/hooks/apiHooks/useSignin.js
+++ src/hooks/apiHooks/useSignin.js
@@ -1,7 +1,9 @@
 import { signinRequest } from "@/api/auth";
 import { useMutation }from "@tanstack/react-query";
+import { useAuth } from "../useAuth";
 
 function useSignIn(){
+    const { setAuth } = useAuth();
     const { isPending, isSuccess, error, mutateAsync } = useMutation({
         mutationFn: signinRequest,
         onSuccess: (response) => {
@@ -13,7 +15,12 @@ function useSignIn(){
                 localStorage.setItem('Token' , response.data.token);
                 const userObject = JSON.stringify(response.data);
                 localStorage.setItem('user' , userObject);
-
+                
+                setAuth({
+                    user: response.data,
+                    Token: response.data.token,
+                    isLoading: false
+                })
                 console.log(response);
             }
         },
