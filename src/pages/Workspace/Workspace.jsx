35f164007d1694b9893e26b1efa78495f336685d
Rupam <rupammondal564@gmail.com>
2024-12-23 12:00:54 +0530

    Updated

--- src/pages/Workspace/Workspace.jsx
+++ src/pages/Workspace/Workspace.jsx
@@ -1,5 +1,6 @@
 import WorkspaceSidebar from "@/components/molecules/WorkspaceSidebar";
 import { useGetWorkspaceId } from "@/hooks/Workspace/useGetWorkspaceId";
+import { LucideLoader2, LucideLoaderCircle, LucideLoaderPinwheel } from "lucide-react";
 import { useParams } from "react-router-dom";
 
 function Workspacepage(){
@@ -9,9 +10,13 @@ function Workspacepage(){
         <>
             <div className="h-[100vh] w-full bg-gradient-to-b from-[#2C2F33] to-[#23272A] flex">
                 <WorkspaceSidebar/>
-                <div className="h-20 w-20 bg-red-500">
-                    {data?.data?.name}
-                </div>
+                {
+                    isFetching && (
+                        <div className="h-20 w-20 bg-red-500">
+                            <LucideLoader2 className="animate-spin"/>
+                        </div>
+                    )
+                }
             </div>
         </>
     )
