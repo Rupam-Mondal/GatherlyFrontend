67b5aef03fc3978ebb95000abb6f1043ef6722be
Rupam <rupammondal564@gmail.com>
2024-12-23 12:02:36 +0530

    Update

--- src/pages/Workspace/Workspace.jsx
+++ src/pages/Workspace/Workspace.jsx
@@ -17,6 +17,13 @@ function Workspacepage(){
                         </div>
                     )
                 }
+                {
+                    isSuccess && (
+                        <div className="h-20 w-20 bg-red-500">
+                            {data?.data?.name}
+                        </div>
+                    )
+                }
             </div>
         </>
     )
