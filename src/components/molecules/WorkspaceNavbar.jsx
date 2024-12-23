19689d7f5d705ffa7a19c100b12d2aa6e27ccfda
Rupam <rupammondal564@gmail.com>
2024-12-23 14:55:04 +0530

    Resizable side bar added"

---
+++ src/components/molecules/WorkspaceNavbar.jsx
@@ -0,0 +1,9 @@
+function WorkspaceNavbar(){
+    return (
+        <>
+            <div className="h-10 w-full bg-[#1F2326]"></div>
+        </>
+    )
+}
+
+export default WorkspaceNavbar;
--- src/pages/Workspace/Workspace.jsx
+++ src/pages/Workspace/Workspace.jsx
@@ -2,28 +2,35 @@ import WorkspaceSidebar from "@/components/molecules/WorkspaceSidebar";
 import { useGetWorkspaceId } from "@/hooks/Workspace/useGetWorkspaceId";
 import { LucideLoader2, LucideLoaderCircle, LucideLoaderPinwheel } from "lucide-react";
 import { useParams } from "react-router-dom";
+import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from '@/components/ui/resizable';
+import WorkspaceNavbar from "@/components/molecules/WorkspaceNavbar";
 
 function Workspacepage(){
     const {workspaceId} = useParams();
     const { isFetching,isSuccess,error,data,refetch } = useGetWorkspaceId(workspaceId);
     return (
         <>
-            <div className="h-[100vh] w-full bg-gradient-to-b from-[#2C2F33] to-[#23272A] flex">
+            <WorkspaceNavbar/>
+            <div className="h-[calc(100vh-40px)] w-full bg-gradient-to-b from-[#2C2F33] to-[#23272A] flex">
                 <WorkspaceSidebar/>
-                {
-                    isFetching && (
-                        <div className="h-20 w-20 bg-red-500">
-                            <LucideLoader2 className="animate-spin"/>
+                <ResizablePanelGroup direction="horizontal" autoSaveId={'workspace-resize'}>
+                    <ResizablePanel
+                        defaultSize={20}
+                        minSize={17}
+                        className='bg-zinc-700'
+                    >
+                        <div>
+                            Sidebar
                         </div>
-                    )
-                }
-                {
-                    isSuccess && (
-                        <div className="h-20 w-20 bg-red-500">
-                            {data?.data?.name}
-                        </div>
-                    )
-                }
+                    </ResizablePanel>
+                    <ResizableHandle withHandle />
+                    <ResizablePanel
+                        minSize={60}
+                        className='bg-zinc-600'
+                    >
+                        some
+                    </ResizablePanel>
+                </ResizablePanelGroup>
             </div>
         </>
     )
