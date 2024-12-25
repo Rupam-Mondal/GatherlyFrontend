45f0be8b543bd657f638a4d1e700e0e0cf495c30
Rupam <rupammondal564@gmail.com>
2024-12-25 09:11:54 +0530

    Loader updated

--- src/pages/Joinpage/Joinpage.jsx
+++ src/pages/Joinpage/Joinpage.jsx
@@ -1,11 +1,12 @@
 import { useGetWorkspaceId } from "@/hooks/Workspace/useGetWorkspaceId";
 import useJoinWorkspace from "@/hooks/Workspace/useJoinWorkspace";
+import { Loader } from "lucide-react";
 import { Navigate, useNavigate, useParams } from "react-router-dom";
 
 function Joinpage() {
     const { workspaceId, joincode } = useParams();
     const { isFetching, isSuccess, error, data, refetch } = useGetWorkspaceId(workspaceId);
-    const { isPending, isSuccess:joinSuccess, error:JoinError, mutateAsync: workspaceJoin } = useJoinWorkspace();
+    const { isPending, isSuccess: joinSuccess, error: JoinError, mutateAsync: workspaceJoin } = useJoinWorkspace();
     const navigate = useNavigate();
 
     const handleJoin = async () => {
@@ -29,12 +30,25 @@ function Joinpage() {
                     <p className="text-lg font-semibold">{data?.data?.name || "Workspace Name"}</p>
                 </div>
                 <div className="flex space-x-4">
-                    <button
-                        onClick={handleJoin}
-                        className="bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded-lg font-semibold transition"
-                    >
-                        Join
-                    </button>
+                    <div>
+                        {
+                            isPending ? (
+                                <button
+                                    onClick={handleJoin}
+                                    className="bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded-lg font-semibold transition"
+                                >
+                                    <Loader className="animate-spin" />
+                                </button>
+                            ) : (
+                                <button
+                                    onClick={handleJoin}
+                                    className="bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded-lg font-semibold transition"
+                                >
+                                    Join
+                                </button>
+                            )
+                        }
+                    </div>
                     <button
                         onClick={handleCancel}
                         className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded-lg font-semibold transition"
