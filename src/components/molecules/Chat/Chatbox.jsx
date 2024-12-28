b28a9933b01fda150007de33bc3c33db81881d76
Rupam <rupammondal564@gmail.com>
2024-12-28 11:30:43 +0530

    Update

--- src/components/molecules/Chat/Chatbox.jsx
+++ src/components/molecules/Chat/Chatbox.jsx
@@ -1,11 +1,13 @@
+import { SendIcon } from "lucide-react";
 import Editor from "./Editor";
 
 function ChatBox(){
     return (
         <>
-            <div className="px-4 w-full">
-                <Editor/>
+            <div className="px-4 w-full relative">
+                <Editor />
             </div>
+
             <div className="flex justify-end px-4">shift + enter to next Line</div>
         </>
     )
--- src/components/molecules/Chat/Editor.jsx
+++ src/components/molecules/Chat/Editor.jsx
@@ -1,10 +1,14 @@
 import React, { useEffect, useRef } from "react";
 import Quill from "quill";
 import "quill/dist/quill.snow.css";
+import { SendIcon } from "lucide-react";
 
 function Editor() {
     const editorRef = useRef(null);
     const quillInstance = useRef(null);
+    function HandleSend(){
+        console.log(quillInstance.current.getContents());
+    }
 
     useEffect(() => {
         if (editorRef.current && !quillInstance.current) {
@@ -13,7 +17,7 @@ function Editor() {
                 modules: {
                     toolbar: [
                         ['bold', 'italic', 'underline', 'strike'],
-                        ['link' , 'image'],
+                        ['link', 'image'],
                         [{ list: 'ordered' }, { list: 'bullet' }],
                         ['clean']
                     ]
@@ -24,8 +28,11 @@ function Editor() {
     }, []);
 
     return (
-        <div className="bg-indigo-200 rounded-2xl">
-            <div ref={editorRef} style={{ height: "80px" }} />
+        <div className="bg-indigo-200 rounded-2xl relative">
+            <div ref={editorRef} style={{ height: "80px", marginRight: "40px" }} />
+            <div className="absolute right-6 top-1/2 -translate-y-1/2 flex items-center cursor-pointer z-10">
+                <SendIcon size={20} className="cursor-pointer" onClick={HandleSend} />
+            </div>
         </div>
     );
 }
