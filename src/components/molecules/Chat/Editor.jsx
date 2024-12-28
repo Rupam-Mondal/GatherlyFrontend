5dfd64dbc05fe3e8dd6f24101ae10228e7a58359
Rupam <rupammondal564@gmail.com>
2024-12-28 15:35:50 +0530

    Update

--- src/components/molecules/Chat/Editor.jsx
+++ src/components/molecules/Chat/Editor.jsx
@@ -6,8 +6,10 @@ import { SendIcon } from "lucide-react";
 function Editor() {
     const editorRef = useRef(null);
     const quillInstance = useRef(null);
-    function HandleSend(){
-        console.log(quillInstance.current.getContents());
+
+    function HandleSend() {
+        const messageObject = JSON.stringify(quillInstance.current.getContents());
+        console.log(messageObject);
     }
 
     useEffect(() => {
@@ -24,12 +26,18 @@ function Editor() {
                 },
                 readOnly: false
             });
+
+            quillInstance.current.on('text-change', () => {
+                const editorElement = editorRef.current;
+                const scrollHeight = editorElement.querySelector('.ql-editor').scrollHeight;
+                editorElement.style.height = `${scrollHeight}px`;
+            });
         }
     }, []);
 
     return (
         <div className="bg-indigo-200 rounded-2xl relative">
-            <div ref={editorRef} style={{ height: "80px", marginRight: "40px" }} />
+            <div ref={editorRef} className="overflow-hidden" style={{ minHeight: "80px", marginRight: "40px" }} />
             <div className="absolute right-6 top-1/2 -translate-y-1/2 flex items-center cursor-pointer z-10">
                 <SendIcon size={20} className="cursor-pointer" onClick={HandleSend} />
             </div>
